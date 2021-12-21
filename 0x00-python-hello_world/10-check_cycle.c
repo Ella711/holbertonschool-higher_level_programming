db744213357c2b58f56b49060db21d4f92267619
Ella711 <allenro711@gmail.com>
2021-12-20 20:01:54 -0500

    [ADD] Files for task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if single or doubly list
+ * @list: pointer to list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	while (temp != NULL)
+	{
+		if (temp->next == list)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);
+}
