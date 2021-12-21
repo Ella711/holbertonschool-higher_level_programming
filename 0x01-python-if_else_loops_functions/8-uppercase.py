e5fa4cca68edc6f73b588267e3b1c40f4416bba1
Ella711 <allenro711@gmail.com>
2021-12-21 16:34:36 -0500

    [FIX] string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     upper_string = ""
-    for i in range(len(str)):
-        char = ord(str[i])
+    for i in str:
+        char = ord(i)
         if 96 < char < 123:
-            upper_string += chr(char - 32)
+            upper_string = chr(char - 32)
         else:
-            upper_string += str[i]
-    print(upper_string)
+            upper_string = i
+        print("{}".format(upper_string), end="")
+    print("")
