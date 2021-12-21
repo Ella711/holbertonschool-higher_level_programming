48e300667207557945d207306c2d17def831ab06
Ella711 <allenro711@gmail.com>
2021-12-21 14:10:17 -0500

    [FIX] styling

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{}{}, ".format(number // 10, number % 10), end="")
     if number == 99:
-        print("{}{}".format(number//10, number % 10))
+        print("{}".format(number))
+    else:
+        print("{:02d}".format(number), end=", ")
