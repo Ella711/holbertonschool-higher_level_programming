b6ca37869f00a4b80f70cd5831e6418b97b81578
Ella711 <allenro711@gmail.com>
2021-12-21 14:04:54 -0500

    [FIX] change format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{:02d}".format(number), end=", ")
+    print("{}{}, ".format(number // 10, number % 10), end="")
     if number == 99:
-        print("{}".format(number))
+        print("{}{}".format(number//10, number % 10))
