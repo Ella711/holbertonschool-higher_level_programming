56ae3f4c63da244e253e57800fbeb53118b5162d
Ella711 <allenro711@gmail.com>
2021-12-23 19:59:43 -0500

    [FIX] - spacing

--- 0x01-python-if_else_loops_functions/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for number in range(0, 100):
-    print("{}{}, ".format(number // 10, number % 10), end="")
-    if number == 99:
-        print("{}{}".format(number//10, number % 10))
