c571227bc916637d070705633e221d2413b230ed
Ella711 <allenro711@gmail.com>
2022-01-03 17:05:13 -0500

    [ADD] - File for task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,4 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if len(tuple_b) == 0:
             tuple_a = (0,)
         tuple_a = (tuple_a[0], 0)
-    return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+    sum_tuple = (tuple_a[0] + tuple_b[0], (tuple_a[1] + tuple_b[1]))
+    return sum_tuple
