1e75ba4950ede264d64c3aa051c0587683cdab5d
Ella711 <allenro711@gmail.com>
2022-01-03 16:58:38 -0500

    [ADD] - File for task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,23 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 2:
         if len(tuple_b) == 1:
-            tuple_b_list = list(tuple_b)
-            tuple_b_list.append(0)
-            tuple_b = tuple(tuple_b_list)
+            tuple_b = (tuple_b[0], 0)
         if len(tuple_b) == 0:
-            tuple_b_list = list(tuple_b)
-            tuple_b_list = [0, 0]
-            tuple_b = tuple(tuple_b_list)
-        return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+            tuple_b = (0, 0)
     elif len(tuple_a) < 2:
         if len(tuple_a) == 1:
-            tuple_a_list = list(tuple_a)
-            tuple_a_list.append(0)
-            tuple_a = tuple(tuple_a_list)
+            tuple_a = (tuple_a[0], 0)
         if len(tuple_a) == 0:
-            tuple_a_list = list(tuple_a)
-            tuple_a_list = [0, 0]
-            tuple_a = tuple(tuple_a_list)
-        return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-    else:
-        return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+            tuple_a = (0, 0)
+    return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
