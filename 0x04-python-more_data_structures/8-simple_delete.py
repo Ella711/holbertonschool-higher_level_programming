045aa509dd2bb4d35f5ee45c9fc2c1427393ef4e
Ella711 <allenro711@gmail.com>
2022-01-05 12:44:23 -0500

    [ADD] - Fixed conditional

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is not None:
-        for entries in a_dictionary:
+        if key in a_dictionary:
             a_dictionary.pop(key, None)
         return a_dictionary
