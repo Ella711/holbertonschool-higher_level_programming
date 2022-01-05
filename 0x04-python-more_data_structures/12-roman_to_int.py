27612b1afa08db5d6ce4465b5443eceb42df2494
Ella711 <allenro711@gmail.com>
2022-01-05 17:04:54 -0500

    [ADD] - File for task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
         'I': 1,
     }
 
-    if roman_string is not None:
+    if roman_string is not None or not isinstance(roman_string, str):
         for key in roman:
             if key in roman_string:
                 idx = roman_string.index(key)
@@ -21,8 +21,8 @@ def roman_to_int(roman_string):
         if idx > 0:
             for letter in roman_string[:idx]:
                 value -= roman.get(letter)
-
         return value
+    return 0
 
 
 
