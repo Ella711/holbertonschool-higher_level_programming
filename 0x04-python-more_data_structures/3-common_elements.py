af3cf8d346494e169dc876b63338a6074ff03dbe
Ella711 <allenro711@gmail.com>
2022-01-05 12:28:52 -0500

    [ADD] - Fixed conditional

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 and set_2 is not None:
+    if set_1 is not None and set_2 is not None:
         commons = set_1.intersection(set_2)
         return commons
     return None
