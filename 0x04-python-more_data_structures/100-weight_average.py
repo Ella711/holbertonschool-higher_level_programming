1240c8742ff579219420550f296d3d422f85ad7e
Ella711 <allenro711@gmail.com>
2022-01-05 23:04:07 -0500

    [ADD] - Fix conditional

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is not None:
-        weight = 0
-        score = 0
-        for pair in my_list:
-            a, b = pair
-            score += a * b
-            weight += b
-        return score / weight
-    else:
+    if not my_list:
         return 0
+
+    weight = 0
+    score = 0
+    for pair in my_list:
+        a, b = pair
+        score += a * b
+        weight += b
+    return score / weight
