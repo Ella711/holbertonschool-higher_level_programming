58b99f8c99ce8661bfd98c221552251fc0fc3cb5
Ella711 <allenro711@gmail.com>
2022-01-18 17:41:12 -0500

    [ADD] - File for task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """Privatize instance attribute: size with optional"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
