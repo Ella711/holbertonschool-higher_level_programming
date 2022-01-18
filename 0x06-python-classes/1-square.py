315563d997a1b09bd14ccd9da35205921948dd9b
Ella711 <allenro711@gmail.com>
2022-01-18 16:40:23 -0500

    [ADD] - File for task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """Privatize instance attribute: size"""
+    def __init__(self, size=0):
+        self.__size = size
