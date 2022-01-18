87909b09ba5ea23140a1c081750e77adfdf420fb
Ella711 <allenro711@gmail.com>
2022-01-17 19:42:16 -0500

    [ADD] - File for task8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
-    if fct is not None:
-        try:
-            return fct(*args)
-        except (TypeError, ValueError, IndexError, ZeroDivisionError) as err:
-            print("Exception: {}\n".format(err), file=sys.stderr)
-            return None
+    try:
+        return fct(*args)
+    except (TypeError, ValueError, IndexError, ZeroDivisionError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
