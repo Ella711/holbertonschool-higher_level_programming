1f3d6d117badb93f6da95d497a6d191d6348c791
Ella711 <allenro711@gmail.com>
2022-01-18 19:30:15 -0500

    [ADD] - File for task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,10 +10,12 @@ class Square:
 
     @property
     def size(self):
+        """Initialize size attribute"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set conditions for size attribute and raise exceptions"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,29 +25,27 @@ class Square:
 
     @property
     def position(self):
+        """Initialize position attribute"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple:
-            self.__position = None
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
-            self.__position = None
+        """Set conditions for position attribute and raise exceptions"""
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int or type(value[1]) is not int:
-            self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
-            self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def area(self):
+        """Find the area of the square"""
         return self.__size ** 2
 
     def my_print(self):
+        """Print a square filled with #"""
         if self.__size == 0:
             return print()
         for i in range(self.__position[1]):
