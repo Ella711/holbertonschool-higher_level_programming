c7b712e9b1da86d8b67a875610a2a0591468444a
Ella711 <allenro711@gmail.com>
2022-01-24 17:51:22 -0500

    [ADD] - File for task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This is the "0-rectangle" module.
+This module supplies one class, Rectangle.
+
+"""
+
+
+class Rectangle:
+    """
+    Class that defines a Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Initialize attributes with optionals: width, height"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Privatize instance attribute: width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set conditions for width attribute and raise exceptions"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Privatize instance attribute: height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set conditions for height attribute and raise exceptions"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of a Rectangle object"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns the perimeter of a Rectangle object"""
+        if self.__height == 0 and self.__width == 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
