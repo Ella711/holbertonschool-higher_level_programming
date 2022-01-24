a9c250c4d583a00acfce98f7b61eb1bc14d2f88a
Ella711 <allenro711@gmail.com>
2022-01-24 17:50:26 -0500

    [ADD] - File for task0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "0-rectangle" module.
+This module supplies one class, Rectangle.
+
+"""
+
+
+class Rectangle:
+    """
+    Empty class that defines a Rectangle
+    """
+    pass
