4865878998e84a0b3e64b3be349891c2d297ea99
Ella711 <allenro711@gmail.com>
2022-01-29 17:38:50 -0500

    [ADD] - Files for task7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the "101-lazy_matrix_mul" module.
+This module supplies one function, lazy_matrix_mul().
+
+"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiplies 2 matrices by using the module NumPy
+    """
+    import numpy as np
+    return np.dot(m_a, m_b)
