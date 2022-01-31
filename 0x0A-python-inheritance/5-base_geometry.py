aea693eee7639d83fee208475ef2140f8e5f9a87
Ella711 <allenro711@gmail.com>
2022-01-31 16:21:56 -0500

    [ADD] - File for task5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "5-base_geometry" module.
+This module supplies one function, base_geometry().
+
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    pass
