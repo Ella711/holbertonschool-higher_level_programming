ef4242a2492694fea9ba4fb8673a40a09c4b10db
Ella711 <allenro711@gmail.com>
2022-01-31 14:00:56 -0500

    [ADD] - File for task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the "0-lookup" module.
+This module supplies one function, lookup().
+
+"""
+
+
+def lookup(obj):
+    """
+    Function that adds 2 integers.
+    """
+    if obj is not None:
+        return dir(obj)
