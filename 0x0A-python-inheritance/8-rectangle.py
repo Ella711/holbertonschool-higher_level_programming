8d5ad18f48d659192b218b00a7b06eff56520264
Ella711 <allenro711@gmail.com>
2022-01-31 17:13:04 -0500

    [ADD] - Files for task8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is the "8-rectangle" module.
+This module supplies one class, Rectangle.
+
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class child of BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Instantiation of private attributes,
+        width and height, validated by integer_validator
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+
