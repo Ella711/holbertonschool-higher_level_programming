dc4c5eeae8f1fdc3f8e49f1b692db4a5d8cd3faa
Ella711 <allenro711@gmail.com>
2022-01-31 17:34:59 -0500

    [ADD] - Files for task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is the "10-square" module.
+This module supplies one class, Square.
+
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Child class of Rectangle
+    """
+    def __init__(self, size):
+        """
+        Instantiation of private attribute, size,
+        validated by integer_validator
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
