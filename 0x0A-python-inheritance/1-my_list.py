9ea775650ebbad1c292858631f91b066c8858cc8
Ella711 <allenro711@gmail.com>
2022-01-31 19:01:02 -0500

    [ADD] - File for task1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,16 +11,8 @@ class MyList(list):
     Class that inherits from list.
     """
 
-    def __init__(self):
-        """
-        Constructor inherited from list
-        """
-        super().__init__()
-
     def print_sorted(self):
         """
         Function that prints a sorted list
         """
-        copy_list = self.copy()
-        copy_list.sort()
-        print(copy_list)
+        print(sorted(self))
