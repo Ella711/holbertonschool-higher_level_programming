f10d39c2289ec2e456ecbfb462beb53d5df872b5
Ella711 <allenro711@gmail.com>
2022-02-01 17:16:52 -0500

    [ADD] - File for task5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the "5-save_to_json_file" module.
+This module supplies one function, save_to_json_file().
+
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Function that writes an Object to a text file,
+    using a JSON representation
+    """
+    with open(filename, "w", encoding='utf-8') as filed_opened:
+        return json.dump(my_obj, filed_opened)
