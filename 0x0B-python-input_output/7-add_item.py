624fa9745c5f735390658f46e313e10f35907b8e
Ella711 <allenro711@gmail.com>
2022-02-01 17:45:42 -0500

    [ADD] - File for task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the "7-add_item" module.
+Script that adds all arguments to a
+Python list, and then save them to a file
+
+"""
+from sys import argv
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+filename = "add_item.json"
+
+try:
+    argv_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    argv_list = []
+
+save_to_json_file(argv_list + argv[1:], "add_item.json")
