8525f931f959cf4bb96292eeaf0e7c3efb3377d2
Ella711 <allenro711@gmail.com>
2022-02-01 19:25:40 -0500

    [ADD] - File for task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the "8-class_to_json" module.
+This module supplies one function, class_to_json().
+
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object
+    """
+    return obj.__dict__
