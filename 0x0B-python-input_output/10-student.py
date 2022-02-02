9bdcc1416198d9e9d2dec1e5ac9a7d8c591b0f05
Ella711 <allenro711@gmail.com>
2022-02-01 20:03:27 -0500

    [ADD] - File for task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This is the "9-student" module.
+This module supplies one class, class Student.
+
+"""
+
+
+class Student:
+    """
+    Defines a student by:
+    Public instance attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor:
+        Public instance attributes:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation
+        of the class. If attrs not in the keys, skip
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    filtered_dict[attr] = self.__dict__[attr]
+            return filtered_dict
