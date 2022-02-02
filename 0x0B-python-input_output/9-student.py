00d32b5e6821d13c85a1cbba9c4283fe50154cb0
Ella711 <allenro711@gmail.com>
2022-02-01 19:39:20 -0500

    [ADD] - File for task9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is the "9-student" module.
+This module supplies one class, class Student.
+
+"""
+
+
+class Student:
+    """
+    Defines a student by:
+    Public instance attributes:
+        first_name
+        last_name
+        age
+
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
