430b9702fb2dddd393f0767af90037305f26c164
Ella711 <allenro711@gmail.com>
2022-03-15 11:13:54 -0500

    [ADD] - File for task17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Script that converts hbtn_0c_0 database to UTF8 and collate utf8mb4 unicode ci
 --  Convert the database, table and field: name
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
