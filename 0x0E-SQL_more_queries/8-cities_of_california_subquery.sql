21e7018b234b94c38a625421aebcc910cb2dd453
Ella711 <allenro711@gmail.com>
2022-03-16 18:57:56 -0500

    [ADD] - File for task8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 -- Results must be sorted in ascending order by cities.id
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id from states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 GROUP BY id
 ORDER BY id;
