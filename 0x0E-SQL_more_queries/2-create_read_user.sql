66abd946dee3ec0cfe0ce5f6ebfe64eb354d32f4
Ella711 <allenro711@gmail.com>
2022-03-16 15:18:09 -0500

    [ADD] - File for task2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2
+-- User should only have SELECT privileges in the new db
+-- pass will be set to user_0d_2_pwd
+-- if database or user already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
