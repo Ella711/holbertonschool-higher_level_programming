ad414405411d5f1e69141ba1ef7ddddf2859226f
Ella711 <allenro711@gmail.com>
2022-03-16 17:19:46 -0500

    [ADD] - File for task6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Script that creates the database hbtn_0d_usa and the table states
+-- Description: id int unique, auto generated, can't be null, primary key
+-- name varchar(256) can't be null
+-- If db or table already exist, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
