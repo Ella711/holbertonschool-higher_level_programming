f2367afb96986e85aca4aa0e4dde4b647e4d6e17
Ella711 <allenro711@gmail.com>
2022-03-16 17:20:59 -0500

    [ADD] - File for task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,7 @@
 -- name varchar(256) can't be null
 -- If db or table already exist, script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT AUTO_INCREMENT NOT NULL UNIQUE,
     name VARCHAR(256) NOT NULL,
