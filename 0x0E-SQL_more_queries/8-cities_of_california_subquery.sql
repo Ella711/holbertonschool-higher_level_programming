279d8614e425674e32b85efe77d24a0e880b97f4
Ella711 <allenro711@gmail.com>
2022-03-16 18:54:46 -0500

    [ADD] - File for task8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Script that lists all the cities of California that can be found in the database hbtn_0d_usa
+-- Results must be sorted in ascending order by cities.id
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id from states WHERE name = 'California')
+GROUP BY id
+ORDER BY id;
