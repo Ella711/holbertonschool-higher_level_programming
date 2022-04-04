4fad638e8f8ddc8fd18e8c8782946241f0784ea8
Ella711 <allenro711@gmail.com>
2022-04-04 14:23:23 -0500

    [ADD] - File for task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const multiLanguage = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+
+while (i < multiLanguage.length) {
+  console.log(multiLanguage[i]);
+  i++;
+}
