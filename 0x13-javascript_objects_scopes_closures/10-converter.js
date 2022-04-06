539eab479cf4ea8b116ab26bae94bce69d564355
Ella711 <allenro711@gmail.com>
2022-04-06 11:26:05 -0500

    [ADD] - File for task10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-exports.converter = (base) => (num) => num.toString(base);
+exports.converter = function (base) {
+  function convert (num) {
+    return num.toString(base);
+  }
+  return convert;
+};
