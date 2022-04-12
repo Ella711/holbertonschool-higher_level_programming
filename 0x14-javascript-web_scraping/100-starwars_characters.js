d86bcb19fc0399285f4647358195b5acf60da1ec
Ella711 <allenro711@gmail.com>
2022-04-12 10:45:52 -0500

    [ADD] - Removed async/await

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,15 +5,15 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(url, async function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     for (const chars in characters) {
-      await request(characters[chars], async (error, response, body) => (error)
+      request(characters[chars], (error, response, body) => (error)
         ? console.log(error)
-        : await console.log(JSON.parse(body).name));
+        : console.log(JSON.parse(body).name));
     }
   } else {
     console.log('Error code:' + response.statusCode);
