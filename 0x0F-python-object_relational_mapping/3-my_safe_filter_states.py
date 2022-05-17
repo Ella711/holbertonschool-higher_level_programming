3fc5941fd08e7b38060282b3694c56d9d3450167
Ella711 <allenro711@gmail.com>
2022-05-17 15:00:25 -0500

    [ADD] - File for Tasks3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-# Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+Same as task2 but is safe from MySQL injections
+"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+
+def main():
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
-    cursor.execute("SELECT * FROM states WHERE name = %(name)s ORDER BY id ASC", {'name': argv[4]})
+    cursor.execute("""SELECT * FROM states
+    WHERE name = %(name)s
+    ORDER BY id ASC""", {'name': argv[4]})
     query_rows = cursor.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     conn.close()
+
+
+if __name__ == "__main__":
+    main()
