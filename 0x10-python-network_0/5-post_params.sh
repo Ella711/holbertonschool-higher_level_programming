03bf17db615f6c258754ced6dfd2e9ce11b69694
Ella711 <allenro711@gmail.com>
2022-05-18 12:28:54 -0500

    [ADD] - Files for tasks0-5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# A variable email must be sent with the value test@gmail.com
+# A variable subject must be sent with the value I will always be here for PLD
+curl -X POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
