ab55db714424a15aaa473c77816af494467c9962
Ella711 <allenro711@gmail.com>
2022-05-18 12:48:48 -0500

    [FIX] - comments

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A variable email must be sent with the value test@gmail.com
-# A variable subject must be sent with the value I will always be here for PLD
+# Script that takes in URL, adds post variables and displays body response
 curl -X POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
