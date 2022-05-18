a4e1c0575935b8ef997c3e2e541479becb08397a
Ella711 <allenro711@gmail.com>
2022-05-18 12:30:48 -0500

    [FIX] - shebang

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 # A variable email must be sent with the value test@gmail.com
 # A variable subject must be sent with the value I will always be here for PLD
