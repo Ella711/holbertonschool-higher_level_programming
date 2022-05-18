6c9d6bd22d1dbce204047dcc1f5ae7b07b214c34
Ella711 <allenro711@gmail.com>
2022-05-18 13:29:34 -0500

    [ADD] - File for task4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in URL, adds header variable and displays body response
-curl -sX "GET" -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
